ef4712407d1799bc1df4e22420a76d2c39188031
sonarsumeet <sumeetsonar1a@gmail.com>
2022-07-10 22:51:36 +0530

    First Branch

--- NetCoreW/Controllers/WeatherForecastController.cs
+++ NetCoreW/Controllers/WeatherForecastController.cs
@@ -34,7 +34,7 @@ namespace NetCoreW.Controllers
                 Summary = Summaries[rng.Next(Summaries.Length)]
             })
             .ToArray();
-            //Commit changes for second branch
+            //Commit changes for first branch
         }
     }
 }
