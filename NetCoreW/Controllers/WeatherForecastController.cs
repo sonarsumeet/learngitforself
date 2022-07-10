39bc293a370df5dda74ccb2ceb470ed756305b5c
sonarsumeet <sumeetsonar1a@gmail.com>
2022-07-10 22:31:09 +0530

    branchOne

--- NetCoreW/Controllers/WeatherForecastController.cs
+++ NetCoreW/Controllers/WeatherForecastController.cs
@@ -26,6 +26,7 @@ namespace NetCoreW.Controllers
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
+            //Make new branch for test
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
